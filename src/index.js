43f592801566110c2a3e6caf497742271452c9ef
Iuliia <iulia.verevkina@mail.ru>
2023-03-14 19:27:41 +0100

    the received data was drawn in the DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {validateIp} from './helpers';
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
 
+const ipInfo = document.getElementById('ip');
+const locationInfo = document.getElementById("location");
+const timezoneInfo = document.getElementById("timezone");
+const ispInfo = document.getElementById("isp");
+
 btn.addEventListener("click", getDate);
 ipInput.addEventListener("keydown", handleKey);
 
@@ -12,7 +17,7 @@ function getDate() {
     `https://geo.ipify.org/api/v2/country?apiKey=at_8IBSV5VS8Qs8ww1Qbjs1WSgWyVbAL&ipAddress=${ipInput.value}`
   )
     .then((response) => response.json())
-    .then(console.log);
+    .then(data => setInfo(data));
 	}
 }
 
@@ -20,4 +25,12 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getDate();
   }
+}
+
+function setInfo(mapData) {
+	console.log(mapData)
+	ipInfo.innerText = mapData.ip;
+	locationInfo.innerText = mapData.location.country + " " + mapData.location.region;
+	timezoneInfo.innerText = mapData.location.timezone;
+	ispInfo.innerText = mapData.isp;
 }
